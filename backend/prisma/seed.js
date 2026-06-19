eb9a1e772a62c66e60a756a52d064c372183da7a
napa708 <68319010008@tatc.ac.th>
2026-06-19 08:28:24 +0700

    EMPLOYEE

--- backend/prisma/seed.js
+++ backend/prisma/seed.js
@@ -4,14 +4,14 @@ const prisma = new PrismaClient();
 async function main() {
   console.log('🌱 กำลังเริ่มฝังข้อมูลตั้งต้น (Database Seeding)...');
 
-  // 0. ล้างข้อมูลเก่าตามลำดับความสัมพันธ์
-  await prisma.user.deleteMany();
+  // 0. ล้างข้อมูลเก่าตามลำดับความสัมพันธ์ (ลบตัวลูกก่อนตัวแม่)
   await prisma.vehicleLog.deleteMany();
   await prisma.attachment.deleteMany();
   await prisma.vehicleBooking.deleteMany();
-  await prisma.vehicle.deleteMany();
   await prisma.roomBooking.deleteMany();
+  await prisma.vehicle.deleteMany();
   await prisma.room.deleteMany();
+  await prisma.user.deleteMany();       // <--- ย้ายการลบ User มาไว้ตรงนี้ (หลังจากลบประวัติการจองหมดแล้ว)
   await prisma.employee.deleteMany();
   await prisma.position.deleteMany();
   await prisma.department.deleteMany();
@@ -72,7 +72,80 @@ async function main() {
     { code: 'SEC001', name: 'สมหมาย รปภ (Guard)', posName: 'จป.เทคนิค', role: 'GUARD' }, // ใช้ตำแหน่งที่ใกล้เคียง รปภ.
     { code: 'ENG001', name: 'สมเกียรติ วิศวกร (User)', posName: 'วิศวกรผลิต', role: 'USER' },
     { code: 'ACC001', name: 'สมศรี บัญชี (User)', posName: 'เจ้าหน้าที่บัญชีและการเงิน', role: 'USER' },
-    { code: 'MC-WK0006', name: 'นายจิระพงค์	ธิบดี', posName: 'หัวหน้าช่างประกอบและติดตั้ง', role: 'USER' },
+
+    { code: 'MC-WK0006', name: 'นายจิระพงค์ ธิบดี', posName: 'หัวหน้าช่างประกอบและติดตั้ง', role: 'USER' },
+    { code: 'MC-WK0010', name: 'นายสายฝน มณีชัย', posName: 'หัวหน้าช่างเชื่อมประกอบ', role: 'USER' },
+    { code: 'MC-WK0012', name: 'นายศรศิริ บุญแก้ว', posName: 'หัวหน้าช่างเชื่อมประกอบ', role: 'USER' },
+    { code: 'MC-WK0334', name: 'นายสุริยันต์ พรมสะอาด', posName: 'หัวหน้าช่างเชื่อมประกอบ', role: 'USER' },
+    { code: 'MC-WK0358', name: 'นายบุญชู กรสันเทียะ', posName: 'หัวหน้าช่างเชื่อมประกอบ', role: 'USER' },
+
+    { code: 'MC-WK0618', name: 'นายกฤษฎา คชเสนีย์', posName: 'วิศวกรผลิต', role: 'USER' },
+    
+    { code: 'MC-WK0609', name: 'นางสาวอาทิตยา ราชบัณฑิต', posName: 'วิศวกรควบคุมคุณภาพ', role: 'USER' },
+    { code: 'MC-EN0477', name: 'นายวราวุธ ซาผู', posName: 'จป.วิชาชีพ', role: 'USER' },
+    { code: 'MC-EN0568', name: 'นายนิติกร หลวงยศ', posName: 'จป.เทคนิค', role: 'USER' },
+    { code: 'MC-WK0620', name: 'นางสาวกัญญารัตน์ เทียมแก้ว', posName: 'จป.วิชาชีพ', role: 'USER' },
+    
+    { code: 'MC-WK0029', name: 'นายชวลิต ยองแสงจันทร์', posName: 'หัวหน้าช่างงานระบบไฟฟ้า', role: 'USER' },
+    { code: 'MC-WK0514', name: 'นางสาวปิยวดี จั้นพลแสน', posName: 'ช่างระบบงานไฟฟ้า', role: 'USER' },
+    { code: 'MC-WK0565', name: 'นายอนุพงศ์ พิมพา', posName: 'ช่างระบบงานไฟฟ้า', role: 'USER' },
+    { code: 'MC-WK0581', name: 'นายเสถียรชัย เชื้อไชยนา', posName: 'ช่างระบบงานไฟฟ้า', role: 'USER' },
+    { code: 'MC-WK0614', name: 'นายธีร์ธวัช ภูมิกอง', posName: 'ช่างระบบงานไฟฟ้า', role: 'USER' },
+
+    { code: 'MC-EN0513', name: 'นางสาวณิชาภัทร วาจาดี', posName: 'เจ้าหน้าที่วิศวกรรมซ่อมบำรุง', role: 'USER' },
+    { code: 'MC-WK0555', name: 'นายวีรศักดิ์ เหล่าลาพระ', posName: 'ช่างซ่อมบำรุง', role: 'USER' },
+    { code: 'MC-WK0639', name: 'นายดนุพร ศุภรมย์', posName: 'ช่างซ่อมบำรุง', role: 'USER' },
+
+    { code: 'MC-PP0291', name: 'นางสาวรุ่งฟ้า มงคลเคหา', posName: 'หัวหน้าแผนกจัดซื้อและสโตร์', role: 'USER' },
+    { code: 'MC-PP0553', name: 'นางสาวทินรัตน์ ผลเพิ่ม', posName: 'เจ้าหน้าที่จัดซื้อ', role: 'USER' },
+    { code: 'MC-AC0299', name: 'นางสาววัชราภรณ์ ภานุรักษ์', posName: 'หัวหน้าแผนกบัญชีและการเงิน', role: 'USER' },
+    { code: 'MC-AC0391', name: 'นางสาวประพิณ เรืองแจ่ม', posName: 'เจ้าหน้าที่บัญชีและการเงิน', role: 'USER' },
+    { code: 'MC-WK0633', name: 'นายพีรดนย์ ทองคำ', posName: 'วิศวกรผลิต', role: 'USER' },
+    { code: 'MC-HR0527', name: 'นายรชต งามตระหง่าน', posName: 'ผจก.ฝ่ายทรัพยากรบุคคลและธุรการ', role: 'USER' },
+    { code: 'MC-HR0608', name: 'นางสาวเบญจวรรณ เอนอ่อน', posName: 'เจ้าหน้าที่ทรัพยากรบุคคลและธุรการ', role: 'USER' },
+    { code: 'MC-WK0622', name: 'นางสาววรรณิภา สินไพร', posName: 'เจ้าหน้าที่ฝ่ายทรัพยากรมนุษย์', role: 'USER' },
+
+    { code: 'MC-EN0286', name: 'นายวรวัฒน์ สุวรรณแก้ว', posName: 'ผู้จัดการแผนกวิศวกรรมไฟฟ้า', role: 'USER' },
+    { code: 'MC-EN0319', name: 'นายบุรินทร์ แจ่มดารา', posName: 'หัวหน้าทีมวิศวกรไฟฟ้า', role: 'USER' },
+    { code: 'MC-WK0325', name: 'นายวุฒมีชัย ตาลหอม', posName: 'วิศวกรไฟฟ้า', role: 'USER' },
+    { code: 'MC-EN0466', name: 'นายวัฒนากร บุญตัน', posName: 'วิศวกรไฟฟ้า', role: 'USER' },
+    { code: 'MC-EN0474', name: 'นายไวยวุฒิ ทวีโยค', posName: 'วิศวกรไฟฟ้า', role: 'USER' },
+    { code: 'MC-EN0538', name: 'นายธรรมรัตน์ รอดจากทุกข์', posName: 'วิศวกรไฟฟ้า', role: 'USER' },
+    { code: 'MC-EN0546', name: 'นาย ภาคภูมิ ชัยรัมย์', posName: 'วิศวกรไฟฟ้า', role: 'USER' },
+    { code: 'MC-EN0547', name: 'ว่าที่ รต. อุดมศักดิ์ อรัญโสตร', posName: 'วิศวกรไฟฟ้า', role: 'USER' },
+
+    { code: 'MC-EN0240', name: 'นาย ทรงวิทย์ ขุนจบเมือง', posName: 'ผจก.ฝ่ายวิศวกรรม', role: 'USER' },
+    { code: 'MC-EN0472', name: 'นาย อัจฉริยะ นวลล่อง', posName: 'วิศวกรการตลาด', role: 'USER' },
+    { code: 'MC-SM0610', name: 'นาย บันลือโลก วงทะนี', posName: 'วิศวกรขายและการตลาด', role: 'USER' },
+
+    { code: 'MC-WK0635', name: 'นาย ศิรศักดิ์ เผือกสงค์', posName: 'เจ้าหน้าที่เขียนแบบ', role: 'USER' },
+
+    { code: 'MC-MK0054', name: 'นาย กฤฐิพงษ์ ศรีศิริ', posName: 'ผจก.ฝ่ายวิศวกรรมการขายและการตลาด', role: 'USER' },
+    { code: 'MC-WK0535', name: 'นางสาว ศิวรักษ์ อินทรแหยม', posName: 'ผู้ช่วยวิศวกรการขายและการตลาด', role: 'USER' },
+    { code: 'MC-SM0597', name: 'นาย วีระชัย ถิ่นฐานทรัพย์', posName: 'Senior Sales and Marketing Manager', role: 'USER' },
+
+    { code: 'MC-EN0320', name: 'นาย วุฒิพงษ์ มาตตี', posName: 'หัวหน้าทีมวิศวกรรมติดตั้งเครื่องจักร', role: 'USER' },
+    { code: 'MC-EN0373', name: 'นาย ศักร์สฤษฏิ์ พรหมฉิม', posName: 'วิศวกรติดตั้งเครื่องจักร', role: 'USER' },
+
+    { code: 'MC-PC0241', name: 'นาย สุชาติ สมโพธิ์', posName: 'หัวหน้าแผนกวิศวกรรมซ่อมบำรุง', role: 'USER' },
+    { code: 'MC-EN0448', name: 'นาย ชนสิษฎ์ มิ่งขวัญ', posName: 'วิศวกรซ่อมบำรุง', role: 'USER' },
+    { code: 'MC-EN0539', name: 'นาย นครินทร์ บุญมาก', posName: 'วิศวกรซ่อมบำรุง', role: 'USER' },
+    { code: 'MC-WK0619', name: 'นาย วันชนะ สังข์ลักษณ์', posName: 'วิศวกรซ่อมบำรุง', role: 'USER' },
+
+    { code: 'MC-EN0326', name: 'นาย สมบูรณ์ กมลภูจิตรคุปต์', posName: 'หัวหน้าทีมวิศวกรสารสนเทศ IT', role: 'USER' },
+    { code: 'MC-EN0494', name: 'นาย พงศ์พัทธ์ แตงอ่อน', posName: 'วิศวกรสารสนเทศ IT', role: 'USER' },
+    { code: 'MC-EN0596', name: 'นาย กิตติ บุญเลิศ', posName: 'วิศวกรสารสนเทศ IT', role: 'USER' },
+    { code: 'MC-EN0644', name: 'นาย รณกฤต เหลืองอ่อน', posName: 'วิศวกรสารสนเทศ IT', role: 'USER' },
+
+    { code: 'MC-EN0386', name: 'นาย วรยุทธ เนตรจ๋อย', posName: 'วิศวกรการออกแบบ', role: 'USER' },
+    { code: 'MC-EN0439', name: 'นาย ศิวดล นนทะภา', posName: 'ผู้ช่วยวิศวกรการออกแบบ', role: 'USER' },
+    { code: 'MC-EN0470', name: 'นาย ทรงศักดิ์ จันทร์คำลา', posName: 'วิศวกรการออกแบบ', role: 'USER' },
+    { code: 'MC-EN0481', name: 'นาย สมพล สุขบรรเทิง', posName: 'วิศวกรการออกแบบ', role: 'USER' },
+
+    { code: 'MC-HR0558', name: 'นาย อาคม ชวนละคร', posName: 'เจ้าหน้าที่งานขนส่ง', role: 'USER' },
+    { code: 'MC-HR0557', name: 'นาย ทองเจือ คำกลาง', posName: 'เจ้าหน้าที่งานขนส่ง', role: 'USER' },
+    { code: 'MC-WK0621', name: 'นาย สามารถ แสนภูมิ', posName: 'เจ้าหน้าที่ขนส่ง', role: 'USER' },
+    { code: 'MC-WK0631', name: 'นาย นิพนธ์ โพธิ์สี', posName: 'เจ้าหน้าที่ขนส่ง', role: 'USER' },
   ];
 
   const createdUsers = [];
